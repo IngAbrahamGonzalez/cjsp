a345d6256b667058e2c7de4c1e6e70aa76f1451e
Abraham Gonzalez <abrahamgonz93@gmail.com>
2023-10-19 14:43:32 -0600

    se agregaron los productos, la funcionalidad de menus desplegables

--- main.js
+++ main.js
@@ -1,12 +1,108 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container')
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleShoppingMenu);
 
 function toggleDesktopMenu() {
     //se ocupa esta funcion para ocultar y mostrar el menu desplegable
-    console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS Funcionando');
+function toggleMobileMenu() {
+    //se ocupa esta funcion para ocultar y mostrar el menu desplegable
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingMenu() {
+    //se ocupa esta funcion para ocultar y mostrar el menu desplegable
+    const isMMClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMMClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+
+});
+productList.push({
+    name: "TV",
+    price: 8500,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    
+});
+productList.push({
+    name: "Xbox",
+    price: 9500,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    
+});
+
+
+function renderProducts (arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.img);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productEmptyDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productEmptyDiv.appendChild(productPrice);
+        productEmptyDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productEmptyDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardContainer.appendChild(productCard);
+    
+    
+    }
+}
+
+renderProducts(productList);
